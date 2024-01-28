d4ca320b8f9f30e3eed18ac2772598a803b6ac7c
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-01-28 13:39:32 -0500

    Worked on the  pet Creation feature

--- VirtualPets/Program.cs
+++ VirtualPets/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter you favourite pet : ");
+            Console.WriteLine(" 1.cat ");
+            Console.WriteLine(" 2.Dog ");
+            Console.WriteLine(" 3.Rabbit ");
+            String petType = Console.ReadLine();
+            Console.WriteLine("Hey, You have Choosen "+ petType);
+            Console.WriteLine("Enter your pet Name :");
+            String petName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + petName + "!");
+        }
+    }
+}
