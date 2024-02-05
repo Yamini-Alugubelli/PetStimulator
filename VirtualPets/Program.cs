51e9628e9ba8a3625dec70aec32f44a710476d67
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-02-05 16:12:04 -0500

    Modified the Code and added comments

--- VirtualPets/Program.cs
+++ VirtualPets/Program.cs
@@ -1,63 +1,49 @@
-﻿using System;
-using System.Buffers.Text;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics.Metrics;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace CSharpTutorials
+﻿namespace CSharpTutorials
 {
     class stimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter you favourite pet : ");
-            Console.WriteLine(" 1.cat ");
-            Console.WriteLine(" 2.Dog ");
-            Console.WriteLine(" 3.Rabbit ");
-            Console.Write("Enter the number of your choice: ");
+            //pet creation
+            Console.WriteLine("Please Enter You Favourite Pet : ");
+            Console.WriteLine(" 1.cat\n 2.Dog\n 3.Rabbit ");
+            Console.Write("Enter The Number Of Your Choice: ");
             int choice = Convert.ToInt32(Console.ReadLine());
-
-  
-            Console.WriteLine("Enter your pet Name :");
+            Console.WriteLine("Enter Your Pet Name :");
             String petName = Console.ReadLine();
-            Console.WriteLine("Welcome, " + petName + "!");
+            Console.WriteLine("Welcome, " + petName.ToUpper() + "!");
+            Pet pet = new Pet(petName, choice);
+            //pet care Actions
             while (true)
             {
                 Console.WriteLine("Please Select Your Action :");
-                //Boolean flag = false;
-                Console.WriteLine("1. Feed");
-                Console.WriteLine("2. Play");
-                Console.WriteLine("3. Rest");
-                Console.WriteLine("4. Status");
-                Console.WriteLine("5. Exit");
+                //Actions
+                Console.WriteLine(" 1. Feed\n 2. Play\n 3. Rest\n 4. Status\n 5. Health Deterioration\n 6. Exit");
                 Console.WriteLine("Enter Your Choice  : ");
                 string action = Console.ReadLine();
-                Pet pet = new Pet(petName, choice);
+               
                 switch (action)
                 {
                     case "1":
-                        pet.Feed();
+                        pet.feed();
                         break;
                     case "2":
-                        pet.Play();
+                        pet.play();
                         break;
                     case "3":
-                        pet.Rest();
+                        pet.rest();
                         break;
                     case "4":
-                        pet.CheckStatus();
+                        pet.status();
                         break;
                     case "5":
-                        Console.WriteLine("Thanks for playing!");
+                        pet.simulateTimePassing();
+                        break;
+                    case "6":
+                        pet.exit();
                         return;
                     default:
-                        Console.WriteLine("Invalid option. Please enter a number between 1 and 5.");
+                        Console.WriteLine("Invalid Option. Please Enter a Number Between 1 and 5.");
                         break;
 
                 }
@@ -70,63 +56,82 @@ namespace CSharpTutorials
 
 class Pet
 {
-    public string Name { get; }
-    public string Type { get; }
-    public int Hunger { get; private set; } = 5;
-    public int Happiness { get; private set; } = 5;
-    public int Health { get; private set; } = 5;
+    //declare variables
+    public string name;
+    public int type;
+    public int hunger;
+    public int happiness;
+    public int health;
 
-    public Pet(string name, int typechoice)
+    //parametrized constructors
+    public Pet(string name, int type)
     {
-        Name = name;
-        switch (typechoice)
+        this.name = name;
+        this.type = type;
+        this.hunger = 5;// initializing variable hunger
+        this.happiness = 5;// initializing variable hapinees
+        this.health = 5;// initializing variable health
+    }
+
+    //feed action implementation
+    public void feed()
+    {
+        hunger--;
+        health++;
+        Console.WriteLine($"{name.ToUpper()} has been fed. Hunger decreased, health increased.");
+    }
+    //play action implementation
+    public void play()
+    {
+        happiness++;
+        hunger++;
+        Console.WriteLine($"{name.ToUpper()} has played. Happiness increased, hunger increased.");
+    }
+    //rest action implementation
+    public void rest()
+    {
+        health++;
+        happiness--;
+        Console.WriteLine($"{name.ToUpper()} has rested. Health increased, happiness decreased.");
+    }
+    //exit action implementation
+    public void exit()
+    {
+        Console.WriteLine($"Thanks for taking care of {name.ToUpper()}! See you next time!");
+    }
+
+    //status action implementation
+    public void status()
+    {
+        Console.WriteLine($"status of {name.ToUpper()}:");
+        Console.WriteLine($"Hunger: {hunger}/10");
+        Console.WriteLine($"Happiness: {happiness}/10");
+        Console.WriteLine($"Health: {health}/10");
+    }
+    //Health Deterioration action implementation
+    public void simulateTimePassing()
+    {
+        Console.WriteLine($"An hour has passed for {name.ToUpper()}.");
+
+        // Increase hunger and decrease happiness over time
+        hunger++;
+        happiness--;
+
+        // Check if neglect consequences
+        if (hunger > 8 || happiness < 3)
         {
-            case 1:
-                Type = "cat";
-                break;
-            case 2:
-                Type = "dog";
-                break;
-            case 3:
-                Type = "rabbit";
-                break;
-            case 4:
-                Type = "bird";
-                break;
-            default:
-                Type = "unknown";
-                break;
+            health--;
+            Console.WriteLine($"{name.ToUpper()}'s health has decreased due to neglect.");
+        }
+
+        // Implement special messages or events based on the pet's status
+        if (hunger > 5)
+        {
+            Console.WriteLine($"{name.ToUpper()} is refusing to play because they're too hungry.");
         }
     }
 
-    public void Feed()
-    {
-        Console.WriteLine($"{Name} is being fed.");
-        Hunger = Math.Max(Hunger - 2, 0);
-        Health = Math.Min(Health + 1, 10);
-    }
-
-    public void Play()
-    {
-        Console.WriteLine($"{Name} is playing.");
-        Happiness = Math.Min(Happiness + 2, 10);
-        Hunger = Math.Min(Hunger + 1, 10);
-    }
-
-    public void Rest()
-    {
-        Console.WriteLine($"{Name} is resting.");
-        Health = Math.Min(Health + 2, 10);
-        Happiness = Math.Max(Happiness - 1, 0);
-    }
-
-    public void CheckStatus()
-    {
-        Console.WriteLine($"Current status of {Name}:");
-        Console.WriteLine($"Hunger: {Hunger}/10");
-        Console.WriteLine($"Happiness: {Happiness}/10");
-        Console.WriteLine($"Health: {Health}/10");
-    }
+    
 }
 
 
